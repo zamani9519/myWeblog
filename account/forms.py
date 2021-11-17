44cd340aa818cc8d83cd519b2e985406f3923c4d
zamani9519 <zamani9519@gmail.com>
2021-11-17 18:06:08 +0330

    برای تغییر دادن متن پیام ارسال شده در ایمیل

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class ProfileForm(forms.ModelForm):
     def __init__(self,*args,**kwargs):
@@ -15,3 +16,11 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username','email','first_name','last_name','special_user','is_author']
+
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
--- account/views.py
+++ account/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView,PasswordChangeView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import logout
+from .models import User
+from .forms import ProfileForm
 from .mixins import (
     FieldMixin,
     FormValidMixin,
@@ -53,7 +56,7 @@ class Profile(UpdateView):
     form_class = ProfileForm
     success_url = reverse_lazy("profile")
     def get_object(self):
-        return User.objects.get(pk=self.request.user.pk)
+        return User.objects.get(pk=2)
     def get_form_kwargs(self):
         kwargs = super(Profile, self).get_form_kwargs()
         kwargs.update({
@@ -70,6 +73,78 @@ class Login(LoginView):
             return reverse_lazy('profile')
 
 
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignupForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+
+class Register(CreateView):
+	form_class = SignupForm
+	template_name = "registration/register.html"
+
+	def form_valid(self, form):
+		user = form.save(commit=False)
+		user.is_active = False
+		user.save()
+		current_site = get_current_site(self.request)
+		mail_subject = 'فعال سازی اکانت'
+		message = render_to_string('registration/activate_account.html', {
+			'user': user,
+			'domain': current_site.domain,
+			'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+			'token':account_activation_token.make_token(user),
+		})
+		to_email = form.cleaned_data.get('email')
+		email = EmailMessage(
+					mail_subject, message, to=[to_email]
+		)
+		email.send()
+		return HttpResponse('لینک فعال سازی به ایمیل شما ارسال شد. <a href="/login">ورود</a>')
+
+def activate(request, uidb64, token):
+	try:
+		uid = force_text(urlsafe_base64_decode(uidb64))
+		user = User.objects.get(pk=uid)
+	except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+		user = None
+	if user is not None and account_activation_token.check_token(user, token):
+		user.is_active = True
+		user.save()
+		return HttpResponse('اکانت شما با موفقیت فعال شد. برای ورود <a href="/login">کلیک</a> کنید.')
+	else:
+		return HttpResponse('لینک فعال سازی منقضی شده است. <a href="/registration">دوباره امتحان کنید.</a>')
+
+# def signup(request):
+#     if request.method == 'POST':
+#         form = SignupForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.is_active = False
+#             user.save()
+#             current_site = get_current_site(request)
+#             mail_subject = 'Activate your blog account.'
+#             message = render_to_string('acc_active_email.html', {
+#                 'user': user,
+#                 'domain': current_site.domain,
+#                 'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+#                 'token':account_activation_token.make_token(user),
+#             })
+#             to_email = form.cleaned_data.get('email')
+#             email = EmailMessage(
+#                         mail_subject, message, to=[to_email]
+#             )
+#             email.send()
+#             return HttpResponse('Please confirm your email address to complete the registration')
+#     else:
+#         form = SignupForm()
+#     return render(request, 'signup.html', {'form': form})
 
 
 
